1044406de96f3c8748a054de25667fc60625c479
Jörn Wagner <joern.wagner@explicatis.com>
2014-03-12 20:06:15 +0100

    Besserer Code

--- src/Acme/DemoBundle/Demo/DirtyClass.php
+++ src/Acme/DemoBundle/Demo/DirtyClass.php
@@ -1,12 +1,16 @@
 <?php
 
-class DirtyClass {
-    public function violatePsr2() {
+class DirtyClass
+{
+    /**
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     * @throws Exception
+     */
+    public function violatePsr2()
+    {
         $i = $j = 0;
-        $j = rand(0,1000);
-        $moreDirty = false;
-        if ($i !== $j)
-        {
+        $j = rand(0, 1000);
+        if ($i !== $j) {
             throw new \Exception('Fail!');
         }
     }
