eb1aa00a9a16b7f36232afd88015c230400d1b08
Jörn Wagner <joern.wagner@explicatis.com>
2014-03-12 18:06:35 +0100

    Noch ein CS-Fehler

--- src/Acme/DemoBundle/Demo/DirtyClass.php
+++ src/Acme/DemoBundle/Demo/DirtyClass.php
@@ -4,6 +4,7 @@ class DirtyClass {
     public function violatePsr2() {
         $i = $j = 0;
         $j = rand(0,1000);
+        $moreDirty = false;
         if ($i !== $j)
         {
             throw new \Exception('Fail!');
